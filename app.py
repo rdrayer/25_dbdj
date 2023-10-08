52aba763b90c5eab9653d60730f13e0294683fdb
Roberta Drayer <robertadrayer@gmail.com>
2023-10-08 16:07:24 -0700

    added all route functionality, with ability to add an existing song to a playlist.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
 #
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -44,8 +44,10 @@ def show_all_playlists():
 @app.route("/playlists/<int:playlist_id>")
 def show_playlist(playlist_id):
     """Show detail on specific playlist."""
-
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    playlist = Playlist.query.get_or_404(playlist_id)
+    songs = playlist.songs
+    return render_template("playlist.html", playlist=playlist, songs=songs)
+    # DONE ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
 
 @app.route("/playlists/add", methods=["GET", "POST"])
@@ -55,8 +57,18 @@ def add_playlist():
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-playlists
     """
+    form = PlaylistForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        new_playlist = Playlist(name=name, description=description)
+        db.session.add(new_playlist)
+        db.session.commit()
+        return redirect('/playlists')
+    
+    return render_template('new_playlist.html', form=form)
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    # DONE ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
 
 ##############################################################################
@@ -75,7 +87,9 @@ def show_all_songs():
 def show_song(song_id):
     """return a specific song"""
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    song = Song.query.get_or_404(song_id)
+    return render_template('song.html', song=song)
+    # DONE ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
 
 @app.route("/songs/add", methods=["GET", "POST"])
@@ -85,29 +99,41 @@ def add_song():
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-songs
     """
+    form = SongForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        artist = form.artist.data
+        new_song = Song(title=title, artist=artist)
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect('/songs')
+    return render_template ('new_song.html', form=form)
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+
+    # DONE ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
 
 @app.route("/playlists/<int:playlist_id>/add-song", methods=["GET", "POST"])
 def add_song_to_playlist(playlist_id):
     """Add a playlist and redirect to list."""
 
-    # BONUS - ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
-
-    # THE SOLUTION TO THIS IS IN A HINT IN THE ASSESSMENT INSTRUCTIONS
-
     playlist = Playlist.query.get_or_404(playlist_id)
     form = NewSongForPlaylistForm()
 
     # Restrict form to songs not already on this playlist
 
-    curr_on_playlist = ...
-    form.song.choices = ...
+    curr_on_playlist = [s.id for s in playlist.songs]
+    form.song.choices = (db.session.query(Song.id, Song.title)
+                        .filter(Song.id.notin_(curr_on_playlist))
+                        .all())
 
     if form.validate_on_submit():
 
           # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+          song = form.song.data
+          add_song = PlaylistSong(playlist_id=playlist_id, song_id=song)
+          db.session.add(add_song)
+          db.session.commit()
 
           return redirect(f"/playlists/{playlist_id}")
 
--- models.py
+++ models.py
@@ -6,21 +6,44 @@ db = SQLAlchemy()
 
 
 class Playlist(db.Model):
+    __tablename__ = 'playlists'
     """Playlist."""
-
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String,
+                     nullable=False,
+                     unique=True)
+    description = db.Column(db.String)
+    songs = db.relationship('Song',
+                            secondary='playlists_songs',
+                            backref='playlists')
 
 
 class Song(db.Model):
+    __tablename__ = 'songs'
     """Song."""
-
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String,
+                      nullable=False)
+    artist = db.Column(db.String,
+                        nullable=False)
 
 
 class PlaylistSong(db.Model):
+    __tablename__ = 'playlists_songs'
     """Mapping of a playlist to a song."""
-
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True)
+    playlist_id = db.Column(db.Integer,
+                            db.ForeignKey("playlists.id"),
+                            primary_key=True)
+    song_id = db.Column(db.Integer,
+                        db.ForeignKey("songs.id"),
+                        primary_key=True)
 
 
 # DO NOT MODIFY THIS FUNCTION
